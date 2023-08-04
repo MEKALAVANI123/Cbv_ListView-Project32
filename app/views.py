c405d66b6a56428c0eb7a99421f9e96254c544be
MEKALAVANI123 <vanimekala4@gmail.com>
2023-08-04 11:09:53 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView
+from app.models import *
+
+class Trainers_List(ListView):
+    model=Trainer
+
+    template_name='Trainers_List.html'
+    context_object_name='tl'
+    #queryset=Trainer.objects.filter(tname='Rakesh')
+    ordering=['tname']
